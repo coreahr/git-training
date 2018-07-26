42eb2dd10ba7eea49e70e88a64cfa438cbb3466a
Minseok Kim <coreahr97@gmail.com>
2018-07-26 14:47:56 +0000

    report card : Add loops
    Signed-off-by: Minseok Kim <coreahr97@gmail.com>

--- 11057_copy0.py
+++ 11057_copy0.py
@@ -5,7 +5,13 @@ N = int(input())
 
 DP = [[1,1,1,1,1,1,1,1,1,1] for i in range(N)]
 
-
+for i in range(N):
+    for j in range(10):
+        if i > 0:
+            DP[i][0] = sum(DP[i-1])
+            DP[i][j] = DP[i][j-1] - DP[i-1][j-1]
+        else:
+            pass
             
 
 #print(sum(DP[N-1])%10007)
